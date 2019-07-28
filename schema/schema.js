e0a5c5b4688dfcb5b392f302859d591e4ca569ba
Don Hansen <donhansen347@gmail.com>
2019-07-28 18:01:11 -0500

    working with add, update, and delete

--- schema/schema.js
+++ schema/schema.js
@@ -1,19 +1,34 @@
 const axios = require('axios');
 const graphql = require('graphql');
 const {
+    GraphQLList,
+    GraphQLNonNull,
     GraphQLObjectType,
     GraphQLSchema,
     GraphQLString
 } = graphql;
 
-// test
-const users = [
-    { email: 'test@test.com', firstName: 'Josh', id: '1', lastName: 'Williams' },
-    { email: 'test2@test.com', firstName: 'Eric', id: '2', lastName: 'James' }
-];
+const PermissionType = new GraphQLObjectType({
+    fields: () => ({
+        id: {
+            type: GraphQLString
+        },
+        name: {
+            type: GraphQLString
+        },
+        users: {
+            resolve(parentValue) {
+                return axios.get(`http://localhost:3001/permissions/${parentValue.id}/users`)
+                    .then((response => response.data));
+            },
+            type: new GraphQLList(UserType)
+        }
+    }),
+    name: 'Permission'
+});
 
 const UserType = new GraphQLObjectType({
-    fields: {
+    fields: () => ({
         email: {
             type: GraphQLString
         },
@@ -25,22 +40,40 @@ const UserType = new GraphQLObjectType({
         },
         lastName: {
             type: GraphQLString
+        },
+        permission: {
+            resolve({ permissionId }) {
+                return axios.get(`http://localhost:3001/permissions/${permissionId}`)
+                    .then(response => response.data);
+            },
+            type: PermissionType
         }
-    },
+    }),
     name: 'User'
 });
 
 const RootQuery = new GraphQLObjectType({
     fields: {
+        permission: {
+            args: {
+                id: {
+                    type: GraphQLString
+                }
+            },
+            resolve(parentValue, { id }) {
+                return axios.get(`http://localhost:3001/permissions/${id}`)
+                    .then(response => response.data);
+            },
+            type: PermissionType
+        },
         user: {
             args: {
                 id: {
                     type: GraphQLString
                 }
             },
-            resolve(parentValue, args) {
-                // return users.find(user => args.id === user.id);
-                return axios.get(`http://localhost:3001/users/${args.id}`)
+            resolve(parentValue, { id }) {
+                return axios.get(`http://localhost:3001/users/${id}`)
                     .then(response => response.data);
             },
             type: UserType
@@ -49,6 +82,70 @@ const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType'
 });
 
+const mutation = new GraphQLObjectType({
+    fields: {
+        addUser: {
+            args: {
+                email: {
+                    type: new GraphQLNonNull(GraphQLString)
+                },
+                firstName: {
+                    type: GraphQLString
+                },
+                lastName: {
+                    type: GraphQLString
+                },
+                permissionId: {
+                    type: GraphQLString
+                }
+            },
+            resolve(parentValue, { email, firstName, lastName, permissionId }) {
+                return axios.post('http://localhost:3001/users', { email, firstName, lastName, permissionId })
+                    .then(response => response.data)
+            },
+            type: UserType
+        },
+        deleteUser: {
+            args: {
+                id: {
+                    type: new GraphQLNonNull(GraphQLString)
+                }
+            },
+            resolve(parentValue, { id }) {
+                return axios.delete(`http://localhost:3001/users/${id}`)
+                    .then(response => response.data);
+            },
+            type: UserType
+        },
+        editUser: {
+            args: {
+                email: {
+                    type: GraphQLString
+                },
+                firstName: {
+                    type: GraphQLString
+                },
+                id: {
+                    type: new GraphQLNonNull(GraphQLString)
+                },
+                lastName: {
+                    type: GraphQLString
+                },
+                permissionId: {
+                    type: GraphQLString
+                }
+            },
+            resolve(parentValue, args) {
+                return axios.patch(`http://localhost:3001/users/${args.id}`, args)
+                    .then(response => response.data);
+            },
+            type: UserType
+        }
+    },
+    name: 'Mutation'
+})
+
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation
 });
